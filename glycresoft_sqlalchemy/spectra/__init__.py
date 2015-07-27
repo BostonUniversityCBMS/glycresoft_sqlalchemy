1d4fae79b52b6c7404830fb73714300ba659d157
Joshua Klein <mobiusklein@gmail.com>
2015-07-27 14:44:01 -0400

    remove odd import

--- glycresoft_sqlalchemy/spectra/__init__.py
+++ glycresoft_sqlalchemy/spectra/__init__.py
@@ -3,8 +3,6 @@ import json
 import logging
 from collections import Iterable
 
-from glypy.utils import opener, pickle
-
 from .spectrum_model import (neutral_mass, mass_charge_ratio)
 
 from .constants import constants
